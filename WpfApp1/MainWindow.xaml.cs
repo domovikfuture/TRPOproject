02b73fd9eca0dbe2cad19bde26821998e62719a5
Adel <prop.het@mail.ru>
2021-05-13 20:54:22 +0300

    Добавлена некоторая проверка полей в окне регистрации.

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -22,7 +22,73 @@ namespace WpfApp1
     {
         public MainWindow()
         {
-            InitializeComponent();
+                InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string login = loginBox.Text.Trim().ToUpper();
+            string []name = nameBox.Text.Trim().Split(" ");
+            string password1 = firstPassBox.Password.Trim();
+            string password2 = secondPassBox.Password.Trim();
+
+            bool isGood = true; 
+            //Проверка поля логина
+            if(login.Length < 5)
+            {
+                isGood = false;
+                loginBox.ToolTip = "Длина логина должна быть от 5 букв!";
+                loginBox.Foreground = Brushes.Red;
+            }
+            else
+            {
+                loginBox.ToolTip = " ";
+                loginBox.Foreground = Brushes.Black;
+            }
+
+            //Поле фио
+            if (name.Length != 3)
+            {
+                isGood = false;
+                nameBox.ToolTip = "Требуется полное написание ФИО!";
+                nameBox.Foreground = Brushes.Red;
+            }
+            else
+            {
+                nameBox.ToolTip = " ";
+                nameBox.Foreground = Brushes.Black;
+            }
+
+            //Проверка пароля
+            if(password1.Length < 6)
+            {
+                isGood = false;
+                firstPassBox.ToolTip = "Длина пароля должна быть от 6 символов!";
+                firstPassBox.Foreground = Brushes.Red;
+            }
+            else
+            {
+                firstPassBox.ToolTip = " ";
+                firstPassBox.Foreground = Brushes.Black;
+            }
+
+            //Подтверждение пароля
+            if(password1 != password2)
+            {
+                isGood = false;
+                secondPassBox.ToolTip = "Пароли не совпадают";
+                secondPassBox.Foreground = Brushes.Red;
+            }
+            else
+            {
+                secondPassBox.ToolTip = " ";
+                secondPassBox.Foreground = Brushes.Black;
+            }
+
+            if(isGood == true)
+            {
+                MessageBox.Show("Ура ты не дебил!");
+            }
         }
     }
 }
