119ae618791005098f2065f38db5400c280cf56d
Adler <prop.het@mail.ru>
2021-06-08 20:14:19 +0300

    Работа с фильтро списка v0.2

--- WpfApp1/Goods.xaml.cs
+++ WpfApp1/Goods.xaml.cs
@@ -18,29 +18,73 @@ namespace WpfApp1
     /// </summary>
     public partial class Goods : Window
     {
+        Criterion crt;
+
         public Goods()
         {
             InitializeComponent();
-            ShowList();
+            ShowList($"select * from Goods");
         }
 
-        private void ShowList()
+        private enum Criterion
         {
-            DataTable talbe = SQLbase.Select($"select * from Goods");
+            more,
+            less,
+            equal
+        }
+
+        private void ShowList(string str)
+        {
+            DataTable talbe = SQLbase.Select(str);
             
             listGoods.ItemsSource = talbe.DefaultView;
-            
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        //private void Button_Click(object sender, RoutedEventArgs e)
+        //{
+        //    DataRowView x = (DataRowView)listGoods.Items[0];
+        //    string s = "";
+        //    foreach (var item in x.Row.ItemArray)
+        //    {
+        //        s += item.ToString()+"*";
+        //    }
+        //    MessageBox.Show(x.Row.ItemArray[0].ToString()+"\n\n"+s);// x.Row.Field<double>("price").ToString());
+        //}
+
+        private void AddGoods(object sender, RoutedEventArgs e)
         {
-            DataRowView x = (DataRowView)listGoods.Items[0];
-            string s = "";
-            foreach (var item in x.Row.ItemArray)
+            int x = listGoods.SelectedIndex;
+
+            
+            if(x == -1)
             {
-                s += item.ToString()+"*";
+                ButtonAdd.ToolTip = "Выберите элемент!";
+                ButtonAdd.Foreground = Brushes.Red;
+                MessageBox.Show(x.ToString());
+                return;
             }
-            MessageBox.Show(x.Row.ItemArray[0].ToString()+"\n\n"+s);// x.Row.Field<double>("price").ToString());
+            else
+            {
+                ButtonAdd.ToolTip = "";
+                ButtonAdd.Foreground = Brushes.LightGreen;
+            }
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            RadioButton pressed = (RadioButton)sender;
+            string s = pressed.Content.ToString();
+
+            if (s == "Меньше")
+            {
+                crt = Criterion.less;
+            }throw new Exception("Незаконченный функционал");
+        }
+
+        private void Reset(object sender, RoutedEventArgs e)
+        {
+            SeatchPrice.Text = "";
+            SearchName.Text = "";
         }
     }
 }
